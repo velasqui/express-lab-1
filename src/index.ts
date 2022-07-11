39e54ea4e94c434bfadc7af2c3c475734e854c3a
Ian velasquez <98791481+velasqui@users.noreply.github.com>
2022-07-11 18:04:42 -0400

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import { Racer } from "./racer";
-import { SolarCar } from "./solarCar";
-import { GasCar } from "./gasCar";
-import { findAverageSpeed, findRacersWithEmptyFuel } from "./racer-functions";
+import express from 'express';
+import cors from 'cors';
+import { cartRouter } from './routes/cartRouter';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/cart-items', cartRouter);
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening on: http://localhost:${port}`));
